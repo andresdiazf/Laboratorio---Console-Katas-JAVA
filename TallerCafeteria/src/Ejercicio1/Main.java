744246bb19ee71a5c940ab9f3e71e93205c83a57
Andrechotech <69160191+andresdiazf@users.noreply.github.com>
2026-04-22 21:02:34 -0500

    probando metodos de formato de moneda en punto 2

--- TallerCafeteria/src/Ejercicio1/Main.java
+++ TallerCafeteria/src/Ejercicio1/Main.java
@@ -1,19 +1,41 @@
+//package Ejercicio1;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        String nombreProducto = "Café";
+//        double precioUnitario = 10.5;
+//        int cantidadInventario = 25000;
+//        boolean disponibleHoy = true;
+//        char codigoProducto = 'A';
+//
+//        System.out.println(String.format("Nombre del producto: %s", nombreProducto));
+//        System.out.println(String.format("Precio unitario: %.2f", precioUnitario));
+//        System.out.println(String.format("Cantidad en inventario: %d", cantidadInventario));
+//        System.out.println(String.format("¿Disponible hoy?: %b", disponibleHoy));
+//        System.out.println(String.format("Código del producto: %c", codigoProducto));
+//    }
+//}
+
 package Ejercicio1;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String nombreProducto = "Café";
-        double precioUnitario = 10.5;
-        int cantidadInventario = 25000;
-        boolean disponibleHoy = true;
+        String nombreProducto = "Tinto";
+        double precioUnitario = 10.50;
+        int cantidadInventario = 20;
+        boolean disponible = true;
         char codigoProducto = 'A';
 
-        System.out.println(String.format("Nombre del producto: %s", nombreProducto));
-        System.out.println(String.format("Precio unitario: %.2f", precioUnitario));
-        System.out.println(String.format("Cantidad en inventario: %d", cantidadInventario));
-        System.out.println(String.format("¿Disponible hoy?: %b", disponibleHoy));
-        System.out.println(String.format("Código del producto: %c", codigoProducto));
+        System.out.println(String.format("Nombre: %s", nombreProducto));
+        System.out.println(String.format("Precio: %.2f", precioUnitario));
+        System.out.println(String.format("Cantidad inventario: %d", cantidadInventario));
+        System.out.println(String.format("Disponible: %b", disponible));
+        System.out.println(String.format("Codigo: %c", codigoProducto));
+
+
+
     }
 }
-
--- TallerCafeteria/src/Ejercicio2/Main.java
+++ TallerCafeteria/src/Ejercicio2/Main.java
@@ -14,3 +14,32 @@ public class Main {
         }
     }
 }
+
+//package ejercicio2;
+//
+//import java.text.NumberFormat;
+//import java.util.Locale;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        double precio = 24000;
+//        Locale colombia = new Locale("es", "CO");
+//        NumberFormat moneda = NumberFormat.getCurrencyInstance(colombia);
+//
+//        double descuento = 0.15;
+//        double valorDescuento = precio * descuento;
+//        double precioTotal = precio - valorDescuento;
+//        double aplicaDescuento = 30000;
+//        boolean tieneDescuento = (precio > aplicaDescuento);
+//
+//
+//        if (tieneDescuento) {
+//            System.out.printf("El precio con descuento es %s", moneda.format(precioTotal));
+//        } else  {
+//            System.out.printf("El precio sin descuento es %s", moneda.format(precioTotal));
+//
+//        }
+//
+//    }
+//}
